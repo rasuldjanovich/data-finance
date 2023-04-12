61568493495e3283ea1de3a87144e99d7d5dfe87
Abdulqodir <rasuldjanovich7@gmail.com>
2023-04-12 11:58:48 +0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Analytics from './components/Analytics';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar/>
       <Hero/>
       <Analytics/>
+      <Newsletter/>
     </div>
   );
 }
